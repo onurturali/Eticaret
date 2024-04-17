8b5959c690810140a068b707262a410db5cbf800
Onur Turalı <onurturali@outlook.com.tr>
2024-04-17 20:52:21 +0300

    sepetteki ürünler

---
+++ Eticaret.Magaza/Controllers/SepetController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Eticaret.Magaza.Controllers
+{
+    [Route("sepet")]
+    public class SepetController : Controller
+    {
+        [Route("")]
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
